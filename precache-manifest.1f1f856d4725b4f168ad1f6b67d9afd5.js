af79a2c711ad106abf962ef2c44fb165ff38f554
Ben Heebner <ben.heebner@gmail.com>
2019-09-02 08:12:56 -0500

    Updates

---
+++ precache-manifest.1f1f856d4725b4f168ad1f6b67d9afd5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97658efc25b3c2a2a3f08f53237d23e8",
+    "url": "/song-matchup/index.html"
+  },
+  {
+    "revision": "8bd475c8a3a64414675c",
+    "url": "/song-matchup/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "a94b7c6f86c2f0b53a6d",
+    "url": "/song-matchup/static/js/2.620b9bfc.chunk.js"
+  },
+  {
+    "revision": "8bd475c8a3a64414675c",
+    "url": "/song-matchup/static/js/main.a7306e1d.chunk.js"
+  },
+  {
+    "revision": "80b1c62c4385c41f64ba",
+    "url": "/song-matchup/static/js/runtime~main.b10fd829.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/song-matchup/static/media/logo.5d5d9eef.svg"
+  }
+]);
